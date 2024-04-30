ef149f18eaf8f8ba5789f48586cfbb1dc6030ba8
Your Name <alessanrospedroso@outlook.com>
2024-04-30 12:29:54 -0300

    Projeto aula 02

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem v-for="(todo,index) in $store.state.todos" :key="index" :todo="todo"/>
+        <!-- <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/> -->
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default{
+    components:{TodoItem}
+}
+
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    todos:[]
+  },
+  getters: {
+  },
+  mutations: {
+    storeTodos(state,payload){
+      state.todos = payload
+      // state.todos.push(payload)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
